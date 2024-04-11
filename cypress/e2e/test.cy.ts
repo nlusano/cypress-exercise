b3f588a58b67cf0e5b109700b589554c6a0cc9e2
Nausicaa <nausicaa.lusano@nedap.com>
2024-04-11 18:15:04 +0200

    Update basic visit test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,9 +1,18 @@
+import { HomePage } from "./pages";
+
 describe("Test", () => {
-  it("should visit the app", () => {
+  it("should visit the app homepage", () => {
     cy.visit("/");
 
     cy.url().should("equal", "https://test-rpg.vercel.app/");
 
     cy.get('[data-testid="hero"] > h1').should("contain", "TestRPG");
+  });
+
+  it("should visit the app homepage but with the tiniest refactor", () => {
+    cy.visit("/");
+
+    HomePage.checkUrl();
+    HomePage.checkTitleIsVisible();
   });
 });
