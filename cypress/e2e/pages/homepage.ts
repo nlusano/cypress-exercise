6e2031fdc3daec48e8c44e82192ae8ee538ba587
Nausicaa <nausicaa.lusano@nedap.com>
2024-04-11 20:24:49 +0200

    Update HomePage class * Added characterName input field selector * Added enterName method

--- cypress/e2e/pages/homepage.ts
+++ cypress/e2e/pages/homepage.ts
@@ -1,13 +1,19 @@
 export class HomePage {
   private constructor() {}
   private static title = '[data-testid="hero"] > h1';
+  private static characterName =
+    '[data-testid="character-card"] input[name="name"]';
   private static url = Cypress.config("baseUrl");
 
-  public static checkTitleIsVisible() {
+  public static checkTitleIsVisible(): void {
     cy.get(this.title).should("contain", "TestRPG");
   }
 
-  public static checkUrl() {
+  public static checkUrl(): void {
     cy.url().should("equal", this.url);
+  }
+
+  public static enterName(name: string): void {
+    cy.get(this.characterName).type(name);
   }
 }
