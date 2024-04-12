314eb10615084cd6b2e1068569e0ad7d1180946e
Nausicaa <nausicaa.lusano@nedap.com>
2024-04-12 07:23:40 +0200

    Add exercises with steps

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -16,3 +16,28 @@ describe("Test", () => {
     HomePage.checkTitleIsVisible();
   });
 });
+
+// LOGIN
+// visit homepage
+// click on login button
+// assert modal visibility
+// enter email
+// assert email field validity using e.g. "aria-invalid" attribute
+// enter password
+// click submit
+// assert "login-store" in localStorage
+// assert modal does not exist anymore
+// assert the logout button now contains "Log out"
+
+// LOGIN error when no email/password are provided
+// click on submit button
+// assert error message email field
+// assert error message password field
+
+// PLAY THE GAME
+// visit /play page
+// assert initial values character name header and stats
+// enter name
+// select build
+// click on start button to play game
+// level up once
